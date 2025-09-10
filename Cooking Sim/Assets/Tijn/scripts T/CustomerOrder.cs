df291195a5e7f1652c8d64048413f13c47da6b5d
TijnDN <tijnden@gmail.com>
2025-09-10 11:38:31 +0200

    order randomizer

---
+++ Cooking Sim/Assets/Tijn/scripts T/CustomerOrder.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // <- needed for new system
+using System.Collections.Generic;
+
+public class CustomerOrder : MonoBehaviour
+{
+    private string[] menuItems = { "Hotdog", "Milkshake", "Fries", "Popcorn", "Chips", "Drinks" };
+    [SerializeField] private int minItems = 1;
+    [SerializeField] private int maxItems = 6;
+
+    void Update()
+    {
+        if (Keyboard.current.oKey.wasPressedThisFrame)
+        {
+            GenerateOrder();
+        }
+    }
+
+    void GenerateOrder()
+    {
+        int itemCount = Random.Range(minItems, maxItems + 1);
+        List<string> order = new List<string>();
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            string randomItem = menuItems[Random.Range(0, menuItems.Length)];
+            order.Add(randomItem);
+        }
+
+        Debug.Log("New Customer Order: " + string.Join(", ", order));
+    }
+}
